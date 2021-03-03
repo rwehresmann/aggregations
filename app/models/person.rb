125eaa042e622f8d6a5ded91bf95ab1ffd455794
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2021-03-03 10:12:16 -0300

    Solve the exercises

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,10 +5,18 @@ class Person < ActiveRecord::Base
   has_many :employees, class_name: "Person", foreign_key: :manager_id
 
   def self.maximum_salary_by_location
-    {}
+    group(:location_id).maximum(:salary)
   end
 
   def self.managers_by_average_salary_difference
-    all
+    avg_salaries_sql = select("manager_id, AVG(salary) AS avg_salary_diff")
+      .group("manager_id")
+      .to_sql
+    
+    joins(
+      "INNER JOIN (#{avg_salaries_sql}) salaries " +
+      "ON  people.id = salaries.manager_id " +
+      "ORDER BY (people.salary - salaries.avg_salary_diff) DESC"
+    )
   end
 end
